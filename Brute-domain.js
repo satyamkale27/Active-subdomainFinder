7516d1a54db99b025c3a7d9480c395677466e5a1
satyam <satyamkale271@gmail.com>
2025-01-12 16:21:21 +0530

    p-queu example

--- Brute-domain.js
+++ Brute-domain.js
@@ -1,15 +1,21 @@
-const dns = require("dns");
+import PQueue from "p-queue";
 
-const promice1 = Promise.resolve(2);
-const promice2 = 42;
-const promice3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "foo");
+const queue = new PQueue({ concurrency: 4 });
+
+const task = async (name, delay) => {
+  console.log(`Starting task ${name}`);
+  await new Promise((resolve) => setTimeout(resolve, delay));
+  console.log(`Completed task ${name}`);
+};
+
+(async () => {
+  await queue.add(() => task("A", 1000));
+  await queue.add(() => task("B", 500));
+  await queue.add(() => task("C", 300));
+  await queue.add(() => task("D", 700));
+  await queue.add(() => task("E", 200));
+})();
+
+queue.onIdle().then(() => {
+  console.log("All tasks completed");
 });
-
-Promise.all([promice1, promice2, promice3])
-  .then((values) => {
-    console.log(values);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
